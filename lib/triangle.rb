f205eaa25f873879f73ee75347620a0c5b090e6e
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-25 10:56:55 +0300

    complited

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+ 
+  def initialize(l1, l2, l3)
+    @l1 =l1
+    @l2 =l2
+    @l3 =l3
+  end
+  def kind
+    if (@l1 <= 0 || @l2 <= 0 || @l3 <= 0)
+      raise TriangleError
+    elsif(@l1 + @l2 <= @l3 || @l2 + @l3 <= @l1 || @l1 + @l3 <= @l2)
+      raise TriangleError
+    elsif (@l1== @l2 && @l2 == @l3)  
+      :equilateral
+    elsif (@l1== @l2 || @l2 == @l3 || @l1 == @l3)  
+      :isosceles 
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "Error"
+    end
+  end
 end
